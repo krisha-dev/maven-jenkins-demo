d0fc095070fcb5b4312c0d675b2fd15ad9253c6d
unknown <KJSCE@16DCEB207C-10.SVV.local>
2026-08-12 15:47:09 +0530

    Initial Maven project

---
+++ src/main/java/HelloMaven.java
@@ -0,0 +1,9 @@
+public class HelloMaven {
+
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("Hello from Maven + Jenkins!");
+        System.out.println("Maven build completed successfully.");
+        System.out.println("=================================");
+    }
+}
